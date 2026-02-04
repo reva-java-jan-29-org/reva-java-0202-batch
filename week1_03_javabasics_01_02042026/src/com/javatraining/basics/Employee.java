78b2dcec3e7b09be7d94781db268b8918693c1e7
vishalshah <suhvishal@gmail.com>
2026-02-04 16:44:29 +0530

    Add notes and ref for week1_day3

---
+++ week1_03_javabasics_01_02042026/src/com/javatraining/basics/Employee.java
@@ -0,0 +1,12 @@
+package com.javatraining.basics;
+
+public class Employee {
+
+	private int id;
+	private String name;
+	private double salary;
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+}
---
+++ week1_03_javabasics_01_02042026/src/com/javatraining/basics/JavaBasics_01.java
@@ -0,0 +1,33 @@
+package com.javatraining.basics;
+
+public class JavaBasics_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Employee employee1 = new Employee();
+		Employee employee2 = new Employee();
+		
+		employee1 = null;
+		
+		int i;
+		
+		
+		byte b;
+		
+		String name;
+
+	}
+	
+	public static void method1() {
+		
+		int a;
+	}
+	
+	public static void method2() {
+		
+		int a;
+		int b;
+	}
+
+}
